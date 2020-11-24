7a99d0afb3bc2192871b759ac86ef866cdf70d3c
Mystic Kitty <djabranton@gmail.com>
2020-11-23 18:45:57 -0800

    Fixed port to 3001

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -46,6 +47,6 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
 }
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
