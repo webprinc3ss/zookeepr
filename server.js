871014219543324112fe41d74ce81fd04189ff30
Mystic Kitty <djabranton@gmail.com>
2020-11-24 12:07:16 -0800

    One more time.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function filterByQuery(query, animalsArray) {
             // Remember, it is initially a copy of the animalsArray,
             // but here we're updating it for each trait in the .forEach() loop.
             // For each trait being targeted by the filter, the filteredResults
-            // array will then contain onl the entries that contain the trait,
+            // array will then contain only the entries that contain the trait,
             // so at the end we'll have an array of animals that have every one 
             // of the traits when the .forEach() loop is finished.
             filteredResults = filteredResults.filter(
